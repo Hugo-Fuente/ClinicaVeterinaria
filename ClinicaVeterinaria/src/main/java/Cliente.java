fd5a1e675ccf42b7787ad2e58d3567f153e0e634
Hugo Gomes <h172386@dac.unicamp.br>
2021-08-10 14:45:28 -0300

    visualCode

---
+++ ClinicaVeterinaria/src/main/java/Cliente.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hugo
+ */
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cep;
+    private String email;
+
+    public Cliente(String nome, String endereco, String telefone, String cep, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cep = cep;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
